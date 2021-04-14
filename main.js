18f98275f0930527303b13e6977fd4adbd3f7c87
priyan <45042030+priyan1995@users.noreply.github.com>
2021-04-14 23:27:39 +0530

    process developed

---
+++ main.js
@@ -0,0 +1,26 @@
+var charactor = document.getElementById("charactor");
+var block = document.getElementById("block");
+
+function jump() {
+    if (charactor.classList != "animate") {
+        charactor.classList.add("animate");
+        setTimeout(function () {
+            charactor.classList.remove("animate");
+        }, 500)
+    }
+}
+
+var checkHit = setInterval(function(){
+    var charactorTop = parseInt(window.getComputedStyle(charactor).getPropertyValue("top"));
+    var blockleft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if(
+        blockleft<20 && blockleft>0 && charactorTop >= 130
+    ){
+        block.style.animation = "none";
+        block.style.direction="none";
+        block.style.background = "black";
+        alert("Looser");
+    }
+
+}, 10)
